74336c4d9a983422c92fa7bce8541a2fc5e33c94
default <isaac.ip@stu.ocsb.ca>
2025-03-06 03:01:54 +0000

    fixed image

--- answer.php
+++ answer.php
@@ -3,9 +3,9 @@
 <html lang="en-ca">
   <head>
     <meta charset="utf-8" />
-    <meta name="description" content="The XXX program, PHP" />
+    <meta name="description" content="The Hello, World! program, PHP" />
     <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Mr. Coxall" />
+    <meta name="author" content="Isaac Ip" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
       rel="stylesheet"
@@ -31,19 +31,19 @@
       href="./favicon-16x16.png"
     />
     <link rel="manifest" href="./site.webmanifest" />
-    <title>The XXX program, PHP</title>
+    <title>The Hello, World! program, PHP</title>
   </head>
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <header class="mdl-layout__header">
         <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The XXX program, PHP</span>
+          <span class="mdl-layout-title">The Hello, World! program, PHP</span>
         </div>
       </header>
       <main class="mdl-layout__content">
         <div class="right-image">
-          <img src="./images/notebook-animation.svg" alt="laptop image" />
+          <img src="./images/coding.gif" alt="nice coding image :)" />
         </div>
         <div class="page-content">The answer is ...</div>
         <br />
--- index.php
+++ index.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
 <!-- ICS2O-Unit1-08-HTML-MDL -->
 <html lang="en-ca">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="The XXX program, PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Mr. Coxall" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The Hello, World! program, PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Isaac Ip" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-indigo.min.css" />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="./apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="./favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="./favicon-16x16.png"
-    />
-    <link rel="manifest" href="./site.webmanifest" />
-<title>The "Hello, World!" program, PHP</title>
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>The "Hello, World!" program, PHP</title>
 </head>
+
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -61,4 +59,5 @@
     </main>
   </div>
 </body>
+
 </html>
